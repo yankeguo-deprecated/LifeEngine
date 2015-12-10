b32f4a51b2e33c14894db54ad125a6f7d68c6b2a
Yanke Guo <guoyk@1mxian.com>
2015-12-11 03:57:07 +0800

    ignore Pods/

--- Example/Pods/Target Support Files/LifeEngine/LifeEngine-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LifeEngine : NSObject
-@end
-@implementation PodsDummy_LifeEngine
-@end
--- Example/Pods/Target Support Files/LifeEngine/LifeEngine-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double LifeEngineVersionNumber;
-FOUNDATION_EXPORT const unsigned char LifeEngineVersionString[];
-
